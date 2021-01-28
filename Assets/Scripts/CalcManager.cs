90e9450651658e24abb485e5f4eabb91574dfe14
Takayuki Ono <takayuki.ono@mail.benesse.co.jp>
2021-01-28 20:34:17 +0900

    init

---
+++ Assets/Scripts/CalcManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CalcManager : MonoBehaviour
+{
+    public Text Count;
+    public Button Good;
+    public Button Reset;
+
+    private int count = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Count.text = "0";
+    }
+
+    public void OnClickGood()
+    {
+        if(count < 10)
+        {
+            count++;
+
+        } else
+        {
+            count = 0;
+        }
+
+        Count.text = count.ToString();
+    }
+
+    public void OnClickResest()
+    {
+        Count.text = "0";
+    }
+}
